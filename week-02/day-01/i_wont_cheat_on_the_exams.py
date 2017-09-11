fd26151acff08e2a37d6b8473d22a86738b561c4
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-11 16:12:21 +0200

    (feat)odd_even complete

--- week-02/day-01/i_wont_cheat_on_the_exams.py
+++ week-02/day-01/i_wont_cheat_on_the_exams.py
@@ -1,2 +1,4 @@
 # Create a program that writes this line 100 times:
 # "I won't cheat on the exam!"
+for text in range(0, 101):
+    print("I won't cheat on the exam!")
--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,2 +1,7 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
+num = int(input())
+if (num % 2) == 0:
+    print("It's even!")
+else:
+    print("It's odd!")
