6c07ed78ddc4637d6de144d27b56950a00a0b4d4
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-11 14:42:37 +0200

    (feat)define_basic_info complete

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,12 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+
+name = 'Balazs Prjevara'
+age = 29
+height = 1.83
+married = False
+print(name)
+print(age)
+print(height)
+print(married)
