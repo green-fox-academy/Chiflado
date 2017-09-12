94a0e9b632cd180639221905c8b5e5ea2b884225
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-12 12:08:52 +0200

    feat(printer): complete

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer(text):
+    print(text)
+
+printer(input('Type your text: '))
