4b220aef2eccf10cbd1e52db9dc32902673e61fe
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-12 16:45:24 +0200

    feat(append_animals): complete

---
+++ week-02/day-02/appends.py
@@ -0,0 +1,10 @@
+
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macsk", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ['kuty', 'makcs', 'cic']
+
+animals = [i + 'a' for i in nimals]
+
+print(animals)
