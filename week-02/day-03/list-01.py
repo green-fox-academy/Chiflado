fead5b21bae87b70f58ee686d608f4344012c636
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-13 10:35:31 +0200

    fix(string-01): missing planets fixed

---
+++ week-02/day-03/list-01.py
@@ -0,0 +1,8 @@
+# Saturn is missing from the planet_list
+# Insert it into the correct position
+
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"]
+
+planet_list = planet_list[:5] + ['Saturn'] + planet_list[5:] + ['PLUTO!!!!!!!']
+
+print(planet_list)
