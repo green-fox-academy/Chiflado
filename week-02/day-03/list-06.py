e6bd3a9bcb05274b6552998972f43fd68062dcbf
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 15:56:11 +0200

    feat(list-06): working

--- week-02/day-03/list-06.py
+++ week-02/day-03/list-06.py
@@ -6,3 +6,11 @@
 list_of_numbers = [2, 4, 6, 8, 10, 12, 14, 16]
 need_to_contain = [4, 8, 12, 16]
 
+def list_checking(need_to_contain):
+    for i in range(len(list_of_numbers)):
+        for j in range(len(need_to_contain)):
+            if not need_to_contain[j] in list_of_numbers:
+                return False
+    return True
+
+print(list_checking(need_to_contain))
