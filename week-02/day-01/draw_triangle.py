ece8938285f01e5ae638563293e3b4e4d804abcb
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 12:10:39 +0200

    feat(guess_the_number) complete

--- week-02/day-01/draw_triangle.py
+++ week-02/day-01/draw_triangle.py
@@ -7,3 +7,9 @@
 # ****
 #
 # The triangle should have as many lines as the number was
+
+triangle_levels = int(input('Levels of the triangle: '))
+
+star = '*'
+for i in range(0 , triangle_levels):
+    print(i * star)
--- week-02/day-01/guess_the_number.py
+++ week-02/day-01/guess_the_number.py
@@ -5,3 +5,17 @@
 # The stored number is higher
 # The stried number is lower
 # You found the number: 8
+
+x = 11
+
+while True:
+    user_guess = int(input('Guess a number: '))
+    if x == user_guess:
+        print('You found the number: 11')
+        break
+    elif x < user_guess:
+        print('The stored number is lower')
+    elif x > user_guess:
+        print('The stored number is higher')
+
+
