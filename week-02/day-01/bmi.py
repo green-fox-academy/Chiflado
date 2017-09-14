d545b563608022239426c621bc5399f82902d4e1
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 12:18:44 +0200

    feat(bmi) complete

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -1,4 +1,10 @@
-massInKg = 81.2
-heightInM = 1.78
+massInKg = 95
+heightInM = 1.83
 
 #Print the Body mass index (BMI) based on these values
+
+def bmi_calulation(massInKg, heightInM):
+    bmi = massInKg / (heightInM ** 2)
+    print(bmi)
+
+bmi_calulation(massInKg, heightInM)
