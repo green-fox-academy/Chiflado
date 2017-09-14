9a8ad79b6a04ded2ac53301b42f8f2f8381af9ba
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 12:40:31 +0200

    feat(cuboid) complete

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+widht = 20
+length = 60
+height = 35
+
+surface_area = 2 * (length * widht + widht * height + height * length)
+volume = length * widht * height
+
+print('Surface Area: ' + str(surface_area))
+print('Volume: ' + str(volume))
