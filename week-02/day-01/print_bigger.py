d639018234bac7712b23a7dacd1ad7f20e220048
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 13:36:16 +0200

    feat(print_bigger): complete

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,10 @@
 # Write a program that asks for two numbers and prints the bigger one
+
+first_number = int(input('First number: '))
+second_number = int(input('Second number: '))
+
+if first_number > second_number:
+    print('The bigger is: ' + str(first_number))
+elif first_number < second_number:
+    print('The bigger is: ' + str(second_number))
+    
