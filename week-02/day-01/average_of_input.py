a55ee127999b1d7ddd4785c840e86ddc265aa82c
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 13:31:11 +0200

    feat(average_of_input): complete

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,9 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+number = 0
+
+for i in range(0, 5):
+    number += int(input('Give me a number: '))
+print('Sum: ' + str(number) + ' Average: ' + str(number/(i + 1)))
