e3a0e23ebf77744f55a9fa5ce54ab8c1bc7a5265
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 15:21:15 +0200

    feat(list-05): complete

---
+++ week-02/day-03/list-05.py
@@ -0,0 +1,10 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1, 2, 3, 4, 5, 6, 8]
+
+for i in numbers:
+    if i == 7:
+        print('Hoooray')
+        break
+if i != 7:
+    print('NOOOOOOO!')
