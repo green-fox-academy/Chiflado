676c2bc9d5c26dcd8db8cea483b7f88c1a212778
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-14 15:46:45 +0200

    fix(list-05): levels_fixed

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -8,3 +8,11 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+
+pyramid_level = int(input('Levels of the pyramid: '))
+
+star = '*'
+empty = ' '
+for i in range(1, pyramid_level*2, 2):
+    y = (pyramid_level * 2 - i // 2)
+    print(y * empty + i * star)
