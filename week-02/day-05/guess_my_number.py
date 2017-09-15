ac5fd30f498f5402310872488a64827116285e7f
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-15 14:36:45 +0200

    feat(guess_the_random_number): added out_of_lifes functions

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -7,6 +7,12 @@ magic_number = randint(1, user_range + 1)
 print('You have 5 lives! \nGuess a number between 1 and ' + str(user_range) + '! ')
 lives = 5
 
+def out_of_lives(lives):
+    if lives == 0:
+        print('Sorry you have no more lifes left, you lost! :(\nThe number was: ' + str(magic_number))
+        return
+
+
 def guess_the_number(magic_number, lives, user_range):
     while lives > 0:
         user_guess = int(input('Please guess a number: '))
@@ -16,15 +22,11 @@ def guess_the_number(magic_number, lives, user_range):
         elif magic_number < user_guess:
             lives -= 1
             print('The number is lower! You have ' + str(lives) + ' lives left')
-            if lives == 0:
-                print('Sorry you have no more lifes left, you lost! :(')
-                return 
+            out_of_lives(lives)
         elif magic_number > user_guess:
             lives -= 1
             print('The number is higher You have ' + str(lives) + ' lives left')
-            if lives == 0:
-                print('Sorry you have no more lifes left, you lost! :(\n The number was: ' + str(magic_number))
-                return
+            out_of_lives(lives)
 
 
 
