272330d39895a7e6252071c78b497a37bd763a4a
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-18 17:37:55 +0200

    feat(copy_file): work

--- week-03/day-01/copy_file.py
+++ week-03/day-01/copy_file.py
@@ -2,3 +2,16 @@
 # It should take the filenames as parameters
 # It should return a boolean that shows if the copy was successful
 
+
+def copy_a_file():
+    first_file = open('my-file.txt', 'r')
+    file_content = first_file.read()
+    second_file = open('copy-of-my-file.txt', 'w+')
+    second_file.write(file_content)
+    copied_content = second_file.read()
+    first_file.close()
+    second_file.close()        
+    if file_content == copied_content:
+        return True
+
+copy_a_file()
