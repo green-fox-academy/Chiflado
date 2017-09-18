2fa8989c5dcb4497ded1f5ea4976b7ceec2d37bd
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-18 14:51:06 +0200

    feat(count_lines): work

---
+++ week-03/day-01/count_lines.py
@@ -0,0 +1,15 @@
+# Write a function that takes a filename as string,
+# then returns the number of lines the file contains.
+# It should return zero if it can't open the file, and
+# should not raise any error.
+
+def line_counter():
+    user_file = input('In which file want you count the lines? ')
+    try:    
+        with open(user_file) as myfile:
+            count = sum(1 for line in myfile)
+            print(str(count) + ' line(s) is/are in the file')
+    except FileNotFoundError:
+        return 0
+
+line_counter()
