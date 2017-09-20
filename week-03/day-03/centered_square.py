06393ff443bd858a20529b8b9cc9b72deae3f61e
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-20 12:13:08 +0200

    feat(centered_square): added variables

--- week-03/day-03/centered_square.py
+++ week-03/day-03/centered_square.py
@@ -7,5 +7,16 @@ canvas.pack()
 
 # draw a green 10x10 square to the center of the canvas.
 
-green_square = canvas.create_rectangle(145, 145, 155, 155, fill= 'green')
+canvas_width = 300
+canvas_height = 300
+edge_length = 10
+
+starting_x = canvas_width / 2 - edge_length / 2
+starting_y = canvas_height / 2 - edge_length / 2
+ending_x = canvas_width / 2 + edge_length / 2
+ending_y = canvas_height / 2 + edge_length / 2
+
+green_square = canvas.create_rectangle( starting_x, starting_y, ending_x, ending_y, fill= 'green')
+
+
 root.mainloop()
