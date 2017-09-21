733859506167db6d9f311586d8a06ba990e0fa7e
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-21 11:14:10 +0200

    feat(number_adder): work

--- week-03/day-04/number_adder.py
+++ week-03/day-04/number_adder.py
@@ -1,1 +1,11 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+def number_adder(num):
+    sum = num
+    if sum <= 0:
+        return sum
+    else:
+        return sum + number_adder(num - 1)
+    
+
+print(number_adder(10))
