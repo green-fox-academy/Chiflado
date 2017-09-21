01552156860879c6a73f163fde9dedf7ca4a11a5
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-21 12:17:56 +0200

    feat(bunny2): work

--- week-03/day-04/bunny2.py
+++ week-03/day-04/bunny2.py
@@ -2,3 +2,14 @@
 # (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 # have 3 ears, because they each have a raised foot. Recursively return the
 # number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+def ears_of_bunnies_and_mutant_bunnies(bunnies):
+    if bunnies == 1:
+        return 2
+    elif bunnies % 2 == 0:
+        return 3 + (ears_of_bunnies_and_mutant_bunnies(bunnies - 1))
+    else:
+        return 2 + (ears_of_bunnies_and_mutant_bunnies(bunnies - 1))
+
+
+print(ears_of_bunnies_and_mutant_bunnies(10))
