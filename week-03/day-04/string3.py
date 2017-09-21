74f1cbf70419322f626bd2ee22cb53a4d9c3db1f
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-21 15:02:22 +0200

    feat(string3): work

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,14 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+def separate_characters(my_string):
+    if my_string == '':
+        return ''
+    elif len(my_string) == 1:
+        return my_string[0]
+    else:
+        temp = my_string[0] + '*'
+        return temp + separate_characters(my_string[1 : ])
+
+
+print(separate_characters('valami baromsag'))
