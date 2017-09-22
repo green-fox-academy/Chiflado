3d41184c73ec8c24d6e3467951babac976748dee
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-22 10:48:53 +0200

    feat(fractal02): finally polygons

--- week-03/day-04/fractal02_again.py
+++ week-03/day-04/fractal02_again.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 root = Tk()
 
@@ -7,8 +8,25 @@ canvas_height = 375
 canvas = Canvas(root, width=canvas_width, height=canvas_height)
 canvas.pack()
 
-top_edge = canvas.create_line(50, 50, 250, 50, fill = 'red')
+def draw_hexagon(x, y, size):
+    hex_height = 3 ** 0.5 / 2 * size
+    canvas.create_polygon(x, y, x + size / 2, y - hex_height,
+                         x + size * 1.5, y - hex_height, 
+                         x + size * 2, y,
+                         x + size * 1.5, y + hex_height,
+                         x + size / 2, y + hex_height, outline = 'black', fill = '')
+    
 
+def draw_freakin_fractal(x, y, size):
+    if size < 5:
+        return
+    else:
+        hex_height = 3 ** 0.5 / 2 * size
+        draw_hexagon(x, y, size)
+        draw_freakin_fractal(x + (size / 2) / 2, y - hex_height / 2, size / 2)
+        draw_freakin_fractal(x + (size / 2) / 2, y + hex_height / 2, size / 2)
+        draw_freakin_fractal(x + (size * 2) / 2, y, size / 2)
 
+draw_freakin_fractal(10, canvas_height / 2, 180)
 
 root.mainloop()
