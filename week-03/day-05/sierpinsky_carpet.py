172bcd47f8828d06be23764ecb821c182e401822
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-24 15:26:35 +0200

    feat(sierpinsky_carpet): woks

---
+++ week-03/day-05/sierpinsky_carpet.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import time
+root = Tk()
+
+canvas_width = 600
+canvas_height = 600
+
+canvas = Canvas(root, width=canvas_width, height=canvas_height)
+canvas.pack()
+
+def draw_rectangle(x, y, size):
+    time.sleep(0.01)
+    canvas.update()
+    canvas.create_rectangle(x + size / 3, y + size / 3, x + size / 3 * 2, y + size / 3 * 2, fill = 'black')
+
+
+def draw_carpet(x, y, size):
+    if size < 3:
+        return
+    else:
+        draw_rectangle(x, y, size) 
+        draw_carpet(x, y, size / 3)
+        draw_carpet(x + size / 3, y, size / 3)
+        draw_carpet(x + size / 3 * 2, y, size / 3)
+        draw_carpet(x, y + size / 3, size / 3)
+        draw_carpet(x+ size / 3 * 2, y + size / 3, size / 3)
+        draw_carpet(x, y + size / 3 * 2, size / 3)
+        draw_carpet(x + size / 3, y + size / 3 * 2, size / 3)
+        draw_carpet(x + size / 3 * 2, y + size / 3 * 2, size / 3)
+
+
+size = canvas_width
+draw_carpet(0, 0, size)
+
+root.mainloop()
