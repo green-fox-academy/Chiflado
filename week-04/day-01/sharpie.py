4a452fe35f683ee01274879cddeffa90b17377b7
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-25 15:00:13 +0200

    feat(sharpie): works

---
+++ week-04/day-01/sharpie.py
@@ -0,0 +1,20 @@
+class Sharpie(object):
+
+    def __init__(self, color, width, ink_amount=float(100)):
+        self.color = str(color)
+        self.width = float(width)
+        self.ink_amount = ink_amount
+
+
+    def use(self):
+        self.ink_amount -= 0.5
+
+
+red_sharpie = Sharpie('red', 0.5)
+print(red_sharpie.color, red_sharpie.width, red_sharpie.ink_amount)
+red_sharpie.use()
+red_sharpie.use()
+red_sharpie.use()
+red_sharpie.use()
+red_sharpie.use()
+print(red_sharpie.color, red_sharpie.width, red_sharpie.ink_amount)
