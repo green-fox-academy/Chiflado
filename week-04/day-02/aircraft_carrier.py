1f40984189570850c372836471bf3efbe7b7762a
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-09-26 17:58:16 +0200

    feat(aircraft_carrier): started refill function

--- week-04/day-02/aircraft_carrier.py
+++ week-04/day-02/aircraft_carrier.py
@@ -9,3 +9,18 @@ class Aircraft(object):
         elif aircraft_type == 'F35':
             self.max_ammo = 12
             self.base_damage = 50
+
+    def fight(self):
+        return self.ammo * self.base_damage
+        self.ammo = 0
+
+    def refill(self, loaded_ammo):
+        return loaded_ammo - self.ammo
+        self.ammo = self.ammo + loaded_ammo
+        if self.ammo > self.max_ammo:
+            self.ammo = self.max_ammo
+
+aircraft1 = Aircraft('F18')
+print(aircraft1.ammo)
+print(aircraft1.refill(10))
+print(aircraft1.ammo)
