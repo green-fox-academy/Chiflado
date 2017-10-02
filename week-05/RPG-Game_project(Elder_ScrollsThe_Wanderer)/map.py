3aeed17c267b761ab02c9717eb96f2ddc04b7be3
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-02 16:04:23 +0200

    feat(Map): added walls

--- week-05/RPG-Game_project(Elder_ScrollsThe_Wanderer)/map.py
+++ week-05/RPG-Game_project(Elder_ScrollsThe_Wanderer)/map.py
@@ -11,20 +11,21 @@ canvas.pack()
 
 field_size = 72
 floor = PhotoImage(file = 'floor.png')
+wall = PhotoImage(file = 'wall.png')
 
 class Map:
 
     def __init__(self):
-        self.level_map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]]
+        self.level_map = [[0, 0, 1, 0, 0, 1, 0, 0, 0, 0,],
+                          [0, 0, 1, 0, 0, 0, 0, 0, 0, 0,],
+                          [0, 0, 1, 0, 0, 0, 0, 1, 1, 0,],
+                          [1, 0, 1, 1, 1, 1, 0, 1, 0, 0,],
+                          [1, 0, 0, 0, 0, 0, 0, 1, 0, 1,],
+                          [0, 0, 1, 0, 0, 0, 0, 1, 0, 0,],
+                          [1, 0, 1, 0, 0, 1, 0, 1, 1, 0,],
+                          [1, 0, 1, 0, 0, 1, 0, 0, 0, 0,],
+                          [1, 0, 1, 1, 1, 1, 0, 1, 1, 1,],
+                          [0, 0, 0, 0, 0, 0, 0, 1, 1, 1,]]
     
     def draw_floor(self):
         for column in range(len(self.level_map)):
@@ -33,8 +34,17 @@ class Map:
                     canvas.create_image(field_size / 2 + row * field_size, 
                     field_size / 2 + column * field_size, image = floor)
 
+    def draw_wall(self):
+        for column in range(len(self.level_map)):
+            for row in range(len(self.level_map)):
+                if self.level_map[column][row] == 1:
+                    canvas.create_image(field_size / 2 + row * field_size, 
+                    field_size / 2 + column * field_size, image = wall)
+        
+
 level_map = Map()
 level_map.draw_floor()
+level_map.draw_wall()
 
 
 root.mainloop()
