1a29f10ff95557b7003f2e0fcf03e0e17e97c221
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-05 10:07:59 +0200

    feat(wanderer): add start stats

--- week-05/RPG-Game_project(Elder_ScrollsThe_Wanderer)/entity.py
+++ week-05/RPG-Game_project(Elder_ScrollsThe_Wanderer)/entity.py
@@ -1,15 +1,38 @@
+from random import randint
 
-class Hero:
+class Entity:
+
+    def __init__(self):
+        self.entity = None
+        self.d6 = randint(1, 6)
+
+    def attributes(self, level = 1, max_hp = 0, defence_point = 0, strike_point = 0):
+        self.level = level
+        self.max_hp = max_hp
+        self.current_hp = self.max_hp
+        self.defence_point = defence_point
+        self.strike_point = strike_point
+
+class Hero(Entity):
 
     def __init__(self):
         self.hero = None
         self.coord_x = 0
         self.coord_y = 0
 
-    def hero_attributes(self):
-        pass
+    def attributes(self):
+        super().attributes(level = 1, max_hp = 20, defence_point = 2, strike_point = 5)
+        self.max_hp = max_hp + 3 * self.d6
+        self.defence_point = defence_point * self.d6
+        self.strike_point = strike_point + self.d6
 
-class Skeleton:
+class Skeleton(Entity):
 
     def __init__(self):
-        self.entity = None
+        super().__init__()
+    
+    def attributes(self):
+        super().attributes(level = 1, max_hp = 2)
+        self.max_hp = max_hp + self.level * self.d6
+        self.defence_point = self.level * self.d6
+        self.strike_point = self.level + self.d6
