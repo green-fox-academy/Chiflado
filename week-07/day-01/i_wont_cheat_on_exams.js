2f25c6741a75a042a7d499a34429b7980a5120a3
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-24 14:06:26 +0200

    feat(i_wont_cheat_on_exam): print with for loop

---
+++ week-07/day-01/i_wont_cheat_on_exams.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let myString = 'I won\'t cheat on the exams!';
+
+for (let i = 0; i < 100; i += 1){
+    console.log(myString);
+}
