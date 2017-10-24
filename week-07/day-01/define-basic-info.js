4fda32e77f750c1d40970477f6d772802fed9689
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-24 12:14:29 +0200

    feat(define-basic-inforamation): print basic informations

---
+++ week-07/day-01/define-basic-info.js
@@ -0,0 +1,15 @@
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Balazs Prjevara';
+let age = 29;
+let height = 183;
+let married = false;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(married);
