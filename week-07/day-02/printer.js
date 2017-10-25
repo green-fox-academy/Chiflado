e98c065102690a82402a98defe540dab3c9a6d41
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-25 12:13:22 +0200

    feat(printer): create printer function

---
+++ week-07/day-02/printer.js
@@ -0,0 +1,11 @@
+
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+
+function printer(...arg){
+    console.log(...arg);
+}
+
+printer('cica', 11, true)
