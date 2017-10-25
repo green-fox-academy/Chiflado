59a27f78976d62b2888c1fc0b210cde107c3f7fb
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-25 14:17:51 +0200

    style(append-a): added a letters

---
+++ week-07/day-02/append-a.js
@@ -0,0 +1,17 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macsk", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+let nimals = ["kuty", "macsk", "cic"];
+
+function append_a(input){
+    let animals = [];
+    input.forEach(function(element, i) {
+        animals.push(element + 'a');
+    });
+    return animals;
+}
+
+console.log(append_a(nimals));
