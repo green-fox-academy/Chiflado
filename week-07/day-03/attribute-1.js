4f3c67bc20dad4d19541678f2611e282a628c552
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 14:42:19 +0200

    feat(attributes): change image

--- week-07/day-03/attribute-1.js
+++ week-07/day-03/attribute-1.js
@@ -1,5 +1,6 @@
 'use strict';
 
-let imageUrl = document.querySelector('img');
+let image = document.querySelector('img');
 
-console.log('Original image\'s URL: ' + imageUrl.src);
+console.log('Original image\'s URL: ' + image.src);
+image.src = 'https://c402277.ssl.cf1.rackcdn.com/photos/7048/images/featured_story/IMG_0281_small.jpg?1403816697';
