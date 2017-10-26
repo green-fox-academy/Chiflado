cddc559f7e06e56338d6ff925961eae4b9bfa398
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 16:10:24 +0200

    feat(classlist-2): add picture

--- week-07/day-03/createelement-1.js
+++ week-07/day-03/createelement-1.js
@@ -5,11 +5,15 @@ let container = document.querySelector('.container');
 let greenFox = document.createElement("li");
 let lampLighter = document.createElement("li");
 let heading = document.createElement('h1');
+let image = document.createElement('img');
+
 
 greenFox.innerText = 'The Green Fox';
 lampLighter.innerText = 'The Lamplighter';
-heading.innerText = 'I can add elements to the DOM!'
+heading.innerText = 'I can add elements to the DOM!';
+image.src = 'https://imgflip.com/s/meme/Welcome-To-The-Internets.jpg';
 
 unordList.appendChild(greenFox);
 unordList.appendChild(lampLighter);
 container.appendChild(heading);
+container.appendChild(image);
