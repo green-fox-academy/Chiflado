fd51300098878996e566a879b28c456cd547f1f3
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 15:30:57 +0200

    feat(classlist): add new class name

--- week-07/day-03/classlist-1.js
+++ week-07/day-03/classlist-1.js
@@ -1,9 +1,13 @@
 'use strict';
 
 let redDot = document.querySelector('.red-dot');
+let cat = document.querySelector('.cat');
+let apple = document.querySelector('.apple');
 
 function alertMessage(){
     alert('OMG DOTS!');
 }
 
 redDot.addEventListener('click', alertMessage);
+cat.innerText = 'dog'
+apple.className += ' red';
