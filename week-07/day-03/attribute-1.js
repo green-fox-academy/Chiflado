d8add74b35751ea0ab53e0448f2e5286df73ad7b
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 15:03:53 +0200

    feat(attributes-1): disable button and add an error message

--- week-07/day-03/attribute-1.js
+++ week-07/day-03/attribute-1.js
@@ -2,7 +2,14 @@
 
 let image = document.querySelector('img');
 let link = document.querySelector('a');
+let secondButton = document.querySelector('.this-one');
 
 console.log('Original image\'s URL: ' + image.src);
 image.src = 'https://c402277.ssl.cf1.rackcdn.com/photos/7048/images/featured_story/IMG_0281_small.jpg?1403816697';
 link.href = 'https://www.greenfoxacademy.com/';
+secondButton.disable = true;
+secondButton.addEventListener('mouseover', disableButton);
+
+function disableButton(){
+  alert('Don\'t even try click, this button is disable');
+}
