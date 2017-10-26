7ded8759d4b947abe9131dc61a066bfebc537572
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 16:05:33 +0200

    feat(classlist-2): add heading to container

--- week-07/day-03/createelement-1.js
+++ week-07/day-03/createelement-1.js
@@ -1,11 +1,15 @@
 'use strict';
 
 let unordList = document.querySelector('ul');
-let greenFox = document.createElement("li")
-let lampLighter = document.createElement("li")
+let container = document.querySelector('.container');
+let greenFox = document.createElement("li");
+let lampLighter = document.createElement("li");
+let heading = document.createElement('h1');
 
-greenFox.innerText = 'The Green Fox'
-lampLighter.innerText = 'The Lamplighter'
+greenFox.innerText = 'The Green Fox';
+lampLighter.innerText = 'The Lamplighter';
+heading.innerText = 'I can add elements to the DOM!'
 
 unordList.appendChild(greenFox);
 unordList.appendChild(lampLighter);
+container.appendChild(heading);
