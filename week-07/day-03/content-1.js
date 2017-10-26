faa6aca3c6d8e4269e7b03ca2c46ed98dd1227b0
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 12:15:12 +0200

    feat(exercise-1): i forgot commiting because i was on elan, sorry

---
+++ week-07/day-03/content-1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let heading = document.querySelector('h1');
+let firstParagraph = document.querySelector('p');
+let secondParagraph = document.querySelector('.other');
+let thirdParagraph = document.querySelector('.result');
+
+
+function alertMessage(){
+    alert('This is an alert message, deal with it');
+}
+
+function logText(input){
+    console.log(input.innerText);
+}
+
+heading.addEventListener('click', alertMessage);
+logText(firstParagraph);
+secondParagraph.addEventListener('click', alertMessage);
+heading.innerText = 'New Content';
+thirdParagraph.innerText = firstParagraph.innerText;
