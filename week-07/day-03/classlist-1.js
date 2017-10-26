d5df6a210bfcd70da1b880a8002e0fcf398b5df9
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 15:35:46 +0200

    refactor(classlist): removed one variable

--- week-07/day-03/classlist-1.js
+++ week-07/day-03/classlist-1.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let redDot = document.querySelector('.red-dot');
-let cat = document.querySelector('.cat');
 let apple = document.querySelector('.apple');
 let balloon = document.querySelector('.balloon');
 
@@ -10,6 +9,6 @@ function alertMessage(){
 }
 
 redDot.addEventListener('click', alertMessage);
-cat.innerText = 'dog'
+redDot.innerText = 'dog'
 apple.className += ' red';
 balloon.style.borderRadius = '0px';
