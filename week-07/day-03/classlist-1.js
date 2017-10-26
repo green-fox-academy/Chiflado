a6204c0421a4a18ffb267646a38d3898032ce403
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 15:34:43 +0200

    feat(classlist): remove border radius

--- week-07/day-03/classlist-1.js
+++ week-07/day-03/classlist-1.js
@@ -3,6 +3,7 @@
 let redDot = document.querySelector('.red-dot');
 let cat = document.querySelector('.cat');
 let apple = document.querySelector('.apple');
+let balloon = document.querySelector('.balloon');
 
 function alertMessage(){
     alert('OMG DOTS!');
@@ -11,3 +12,4 @@ function alertMessage(){
 redDot.addEventListener('click', alertMessage);
 cat.innerText = 'dog'
 apple.className += ' red';
+balloon.style.borderRadius = '0px';
