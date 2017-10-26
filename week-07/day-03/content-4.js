b3dda75a025e11f9969759ea79e7c8390edf9f7a
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 14:14:04 +0200

    feat(exercise-4): set the background color

--- week-07/day-03/content-4.js
+++ week-07/day-03/content-4.js
@@ -2,7 +2,10 @@
 
 let placeholders = ['apple', 'banana', 'cat', 'dog'];
 let listElements = document.querySelectorAll('li');
+let listBlock = document.querySelector('ul');
 
 for (let i = 0; i < listElements.length; i++){
     listElements[i].innerText = placeholders[i];
 }
+
+listBlock.style.background = 'limegreen';
