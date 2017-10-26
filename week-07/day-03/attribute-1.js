72d7f276ea506ced8994af1aae8ff6281cef486a
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-26 14:45:34 +0200

    feat(attributes): change link url

--- week-07/day-03/attribute-1.js
+++ week-07/day-03/attribute-1.js
@@ -1,6 +1,8 @@
 'use strict';
 
 let image = document.querySelector('img');
+let link = document.querySelector('a');
 
 console.log('Original image\'s URL: ' + image.src);
 image.src = 'https://c402277.ssl.cf1.rackcdn.com/photos/7048/images/featured_story/IMG_0281_small.jpg?1403816697';
+link.href = 'https://www.greenfoxacademy.com/';
