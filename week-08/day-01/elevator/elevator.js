e9d020eb2e8e5a10fcc8bc6a1c1df1bf3591f180
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-10-30 16:02:54 +0100

    feat(elevator): create decreaseLevel method, tested

--- week-08/day-01/elevator/elevator.js
+++ week-08/day-01/elevator/elevator.js
@@ -1,6 +1,7 @@
 
 let levelList = document.querySelectorAll('.floor');
 let upButton = document.querySelector('.up')
+let downButton = document.querySelector('.down')
 
 class SwitchLevels{
     constructor(){
@@ -18,11 +19,24 @@ class SwitchLevels{
         }
         this.drawFloor();
     }
+    decreaseLevel(){
+        if (this.level > 0){
+            for (let i = 0; i < levelList.length; i++){
+                if (levelList[i]==document.querySelector('.active')){
+                    levelList[i].className = 'floor'
+                    this.level -= 1;
+                }
+            }
+        }
+        this.drawFloor();
+    }
     drawFloor(){
-        levelList[this.level].className += ' active';
+        levelList[this.level].className = 'floor active';
     }
 }
 
 let elevator = new SwitchLevels;
 let goingUp = elevator.increaseLevel.bind(elevator);
+let goingDown = elevator.decreaseLevel.bind(elevator);
 upButton.addEventListener('click', goingUp);
+downButton.addEventListener('click', goingDown);
