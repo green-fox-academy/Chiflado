0d9447f989d65d70ba98687d3bd63e86c805b582
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-02 16:38:34 +0100

    feat(moonlanding): create renderArticle fucntion and rendered

--- week-08/day-04/moonlanding/moonlanding.js
+++ week-08/day-04/moonlanding/moonlanding.js
@@ -1,13 +1,25 @@
 'use strict';
 
-function getArticle(){
+function getArticle(callback){
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=1fc7a7dbfaee49a7b9012a13b9bee39b&begin_date=19690719&end_date=19690720');
     xhr.onload = function(){
         let articles = JSON.parse(xhr.responseText);
-        console.log(articles);
+        callback(articles);
     }
     xhr.send();
 }
 
-getArticle();
+function renderArticle(input){
+    let h1 = document.createElement('h1');
+    let snippet = document.createElement('article');
+    let pubDate = document.createElement('p');
+    h1.textContent = input.response.docs[5].headline.main;
+    snippet.textContent = input.response.docs[5].snippet;
+    pubDate.textContent = 'Published: ' + input.response.docs[5].pub_date;
+    document.body.appendChild(h1);
+    document.body.appendChild(snippet);
+    document.body.appendChild(pubDate);
+}
+
+getArticle(renderArticle);
