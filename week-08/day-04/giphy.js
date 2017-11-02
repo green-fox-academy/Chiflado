b4f5099688402ba58f9b7f4b83fcd63dd5a05c14
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-02 14:29:41 +0100

    feat(giphy API): set one image with callback

--- week-08/day-04/giphy.js
+++ week-08/day-04/giphy.js
@@ -1,13 +1,20 @@
 'use strict';
 
-function getGIF(){
+let thumbnails = document.querySelectorAll('div');
+
+function getGIF(callback){
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=lTIMCZkccb0VcetN359mqKfkXkdKNHI9&q=star wars&limit=16&offset=16&rating=R&lang=en');
+    xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=lTIMCZkccb0VcetN359mqKfkXkdKNHI9&q=starwars&limit=16&offset=16&rating=R&lang=en');
     xhr.onload = function(){
         let gifs = JSON.parse(xhr.responseText);
-        console.log(gifs);
+        // console.log(gifs.data[0].images.fixed_height.url);
+        callback(gifs);
     }
     xhr.send();
-}
+};
 
-getGIF();
+function showThumbnails(jsonData){
+    thumbnails[0].setAttribute('style', 'background: url('+jsonData.data[0].images.fixed_height.url+'); background-size: cover');
+};
+
+getGIF(showThumbnails);
