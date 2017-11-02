0ece1428f9b1adc2dfa54d8a79634ed1e1f6eeb6
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-02 16:46:31 +0100

    feat(moonlanding): create permalink to original article

--- week-08/day-04/moonlanding/moonlanding.js
+++ week-08/day-04/moonlanding/moonlanding.js
@@ -5,6 +5,7 @@ function getArticle(callback){
     xhr.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=1fc7a7dbfaee49a7b9012a13b9bee39b&begin_date=19690719&end_date=19690720');
     xhr.onload = function(){
         let articles = JSON.parse(xhr.responseText);
+        console.log(articles);
         callback(articles);
     }
     xhr.send();
@@ -14,12 +15,20 @@ function renderArticle(input){
     let h1 = document.createElement('h1');
     let snippet = document.createElement('article');
     let pubDate = document.createElement('p');
+    let permalink = document.createElement('a');
+
     h1.textContent = input.response.docs[5].headline.main;
     snippet.textContent = input.response.docs[5].snippet;
+    permalink.textContent = 'Read More';
+    permalink.href = input.response.docs[5].web_url;
     pubDate.textContent = 'Published: ' + input.response.docs[5].pub_date;
+
     document.body.appendChild(h1);
     document.body.appendChild(snippet);
+    document.body.appendChild(permalink);
     document.body.appendChild(pubDate);
+
+
 }
 
 getArticle(renderArticle);
