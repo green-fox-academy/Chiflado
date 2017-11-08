c112b0c0107b7fa7c2efb7a45dba8204ff6838c1
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-08 16:53:34 +0100

    feat(backend): create doUntil function, tested

--- week-09/day-03/backend.js
+++ week-09/day-03/backend.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 app.use('/assets', express.static('./assets'));
 express.json.type = "application/json";
+app.use(express.json());
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -34,5 +35,28 @@ app.get('/appenda/:appendable', function(req, res){
     res.json({appended : req.params.appendable + "a"})
 });
 
+app.post('/dountil/:item', function(req, res) {
+    if ( req.params.item == "sum" ) {
+        let number = req.body.until;
+        let result = 0;
+        while (number >= 0){
+            result += number;
+            number -= 1;
+        }
+        res.json({"result": result}); 
+    } else if ( req.params.item == "factor" ) {
+        let number = req.body.until;
+        let result = 1;
+        while (number > 0){
+            result *= number;
+            number -= 1;
+        }
+        res.json({"result": result});    
+     } else {
+        res.json({
+            "error": "Please provide a number!"
+        });    
+    }    
+});    
 
 app.listen(8080);
