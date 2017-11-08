c85f8d49c1433a68105b8d77a06068b91b40a7b0
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-08 13:46:34 +0100

    feat(daily_project): create doubling function, tested

--- week-09/day-03/backend.js
+++ week-09/day-03/backend.js
@@ -4,10 +4,18 @@ var express = require('express');
 
 var app = express();
 app.use('/assets', express.static('./assets'));
+express.json.type = "application/json";
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/doubling', function(req, res){
+    if(req.query.input === undefined){
+        res.json({error : "Please provide an input!"});
+    } else {
+        res.json({received : req.query.input, result : req.query.input*2})
+    }
+});
 
 app.listen(8080);
