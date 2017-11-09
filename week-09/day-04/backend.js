47cc1f8a23025eb92b8adb23a7bfd61e7405e44f
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-09 12:22:54 +0100

    feat(backend): create html file and add to backend

--- week-09/day-04/backend.js
+++ week-09/day-04/backend.js
@@ -5,6 +5,13 @@ var express = require('express');
 var port = 3000;
 
 var app = express();
+express.json.type = "application/json";
+app.use(express.json());
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 app.listen(port);
 console.log('the server run at: http://localhost:' + port);
