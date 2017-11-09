c0524f7676df671ec2f84ac0bcb6031042b7e065
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-09 15:38:16 +0100

    feat(frontend): create ajaxCall function

--- week-09/day-04/assets/frontend.js
+++ week-09/day-04/assets/frontend.js
@@ -1,3 +1,14 @@
 'use strict';
 
 var url = 'http://localhost:3000/';
+
+function ajaxCall(command, endPoint, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open(command, url + endPoint);
+    xhr.onload = function() {
+        let data = JSON.parse(xhr.responseText);
+        callback(data)
+    };
+    xhr.send();
+};
+
--- week-09/day-04/backend.js
+++ week-09/day-04/backend.js
@@ -10,27 +10,32 @@ express.json.type = "application/json";
 app.use(express.json());
 app.use('/assets', express.static('./assets'));
 
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'root',
+    database : 'bookstore'
+});
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-var connection = mysql.createConnection({
-                    host : 'localhost',
-                    user : 'root',
-                    password : 'root',
-                    database : 'bookstore'
-                });
+app.get('/book_titles', function(req, res){
+    connection.query('SELECT book_name FROM book_mast;', function(err, data){
+        console.log(data);
+        res.json(data);
+    });
+});
+
 
 connection.connect(function(err){
     if(err){
         console.log("Error connecting to Database");
     }
     console.log("Connection established");
-    });
+});
 
-connection.query('SELECT * FROM book_mast;', function(err, data){
-    console.log(data);
-})
 
 app.listen(port);
 console.log('the server run at: http://localhost:' + port);
