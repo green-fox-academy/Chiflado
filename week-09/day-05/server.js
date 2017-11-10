ca73ac6519073ff53b04f315a834ecdf21e1d555
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-10 12:10:04 +0100

    feat(submit): create a redirect to front page

--- week-09/day-05/server.js
+++ week-09/day-05/server.js
@@ -14,5 +14,13 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/submit.html', function(req, res){
+    res.sendFile(__dirname + '/submit.html');
+});
+
+app.get('/index.html', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(port);
 console.log('the server run at: http://localhost:' + port);
