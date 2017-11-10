19c9295210c5ec20f0b13ce44e11710a89c899ed
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-10 10:21:03 +0100

    (daily_project): moved files to the folder

---
+++ week-09/day-05/assets/reddit.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let postContainer = document.querySelector('.post_container');
+
+function createPost(object){
+    let postElements =`<div class="votes">
+                        <div class="upvote"></div>
+                        <div class="counter">${object.score}</div>
+                        <div class="downvote"></div>
+                        </div>
+                        <a class="content", href="${object.url}">${object.title}</a>`;
+    let post = document.createElement('article')
+    post.className = 'post';
+    post.dataset.ID = object.id;
+    post.innerHTML = postElements;
+    postContainer.appendChild(post);
+    return post;
+}
+
+
+function doRequest(callback) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://secure-reddit.herokuapp.com/simple/posts');
+    x.onload = function() {
+        let data = JSON.parse(x.responseText);
+        callback(data)
+    };
+    x.send();
+  }
+
+function renderPosts(data){
+    for(let i = 0; i < data.posts.length; i++){
+        let postDom = createPost(data.posts[i]);
+        let upvote = postDom.querySelector('.upvote')
+        console.log(upvote);          
+    }
+}
+
+function handleData(data){
+    console.log(data);
+}
+
+doRequest(handleData);
+doRequest(renderPosts);
