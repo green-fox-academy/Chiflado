8f8f9fe1313329b1a8a3d9c55590ff7de02a53e1
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-10 10:08:47 +0100

    feat(frontend): create table with infos

--- week-09/day-04/assets/frontend.js
+++ week-09/day-04/assets/frontend.js
@@ -2,6 +2,15 @@
 
 var url = 'http://localhost:3000/';
 
+function ajaxCall(command, endPoint, callback){
+    let xhr = new XMLHttpRequest();
+    xhr.open(command, url + endPoint);
+    xhr.onload = function() {
+        let data = JSON.parse(xhr.responseText);
+        callback(data)
+    };
+    xhr.send();
+};
 
 function createList(data){
     let listContainer = document.createElement('ul');
@@ -13,20 +22,26 @@ function createList(data){
     }
 };
 
-function ajaxCall(command, endPoint, callback){
-    let xhr = new XMLHttpRequest();
-    xhr.open(command, url + endPoint);
-    xhr.onload = function() {
-        let data = JSON.parse(xhr.responseText);
-        callback(data)
-    };
-    xhr.send();
-};
-
+function createPost(data){
+    let table = document.createElement('table');
+    document.querySelector('body').appendChild(table);
+    for (let i = 0; i<data.length; i++){
+        let postElements =`<td>${data[i].book_name}</td>
+                           <td>${data[i].aut_name}</td>
+                           <td>${data[i].cate_descrip}</td>
+                           <td>${data[i].pub_name}</td>
+                           <td>${data[i].book_price}</td>`;
+        let bookDatas = document.createElement('tr');
+        bookDatas.innerHTML = postElements;
+        table.appendChild(bookDatas);
+    }
+}
+        
 function handleData(data){
     console.log(data);
 }
 
 
 ajaxCall('GET', 'book_infos', handleData);
-ajaxCall('GET', 'book_titles', createList);
+// ajaxCall('GET', 'book_titles', createList);
+ajaxCall('GET', 'book_infos', createPost);
