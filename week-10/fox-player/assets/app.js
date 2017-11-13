0127e461994322c7620227337a9d631bcc0e3677
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-13 17:26:11 +0100

    feat(app.js): create JS file, changing start and pause buttons

--- week-10/fox-player/assets/app.js
+++ week-10/fox-player/assets/app.js
@@ -6,8 +6,12 @@ let play = document.querySelector('.play');
 play.addEventListener('click',function(event){
     if(audio.paused){
         audio.play();
+        play.setAttribute('style', `background: url("assets/pause.svg"); 
+                                    background-repeat: no-repeat; background-position: center`)
     }
     else{
         audio.pause();
+        play.setAttribute('style', 'background: url("assets/play.svg")')
+        
     }
 }, false);
