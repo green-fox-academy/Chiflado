9cce16b49f455643d23ce55d0143aace65bef16e
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-11-14 14:39:55 +0100

    feat(app): moving seek bar

--- week-10/fox-player/assets/app.js
+++ week-10/fox-player/assets/app.js
@@ -1,17 +1,41 @@
 'use strict';
 
-let audio = document.querySelector('audio');
+let music = document.querySelector('audio');
 let play = document.querySelector('.play');
+let duration = music.duration;
+let playhead = document.querySelector('#playhead');
+let timeline = document.querySelector('#timeline'); 
+let timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
 
 play.addEventListener('click',function(event){
-    if(audio.paused){
-        audio.play();
+    if(music.paused){
+        music.play();
         play.setAttribute('style', `background: url("assets/pause.svg"); 
                                     background-repeat: no-repeat; background-position: center`)
     }
     else{
-        audio.pause();
+        music.pause();
         play.setAttribute('style', 'background: url("assets/play.svg")')
         
     }
+}, false); 
+
+music.addEventListener("timeupdate", timeUpdate, false);
+
+function clickPercent(event) {
+    return (event.clientX - getPosition(timeline)) / timelineWidth;
+}
+
+function timeUpdate() {
+    let playPercent = timelineWidth * (music.currentTime / duration);
+    playhead.style.marginLeft = playPercent + "px";
+    if (music.currentTime == duration) {
+        play.className = "";
+        play.className = "play";
+    }
+}
+
+
+music.addEventListener("canplaythrough", function() {
+    duration = music.duration;
 }, false);
