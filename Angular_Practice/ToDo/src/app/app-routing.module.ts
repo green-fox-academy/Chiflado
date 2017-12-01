98c5eeaf3cb3c9e7a3d9d560c06ef88507556ab6
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-12-01 15:07:08 +0100

    feat(app-routing.module): import ToDoComponent and add routes

--- Angular_Practice/ToDo/src/app/app-routing.module.ts
+++ Angular_Practice/ToDo/src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ToDoComponent } from './to-do/to-do.component';
+
+const routes: Routes = [
+  {path: 'todos', component: ToDoComponent}
+]
+
 @NgModule({
   imports: [
     RouterModule
