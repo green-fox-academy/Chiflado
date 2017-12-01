52874286d3e778ae803280f66de2c90fc2b0ae34
Balazs Prjevara <prjevarabalazs@gmail.com>
2017-12-01 14:10:53 +0100

    feat(to-do.component.ts): import mocked and add todos property

--- Angular_Practice/ToDo/src/app/to-do/to-do.component.ts
+++ Angular_Practice/ToDo/src/app/to-do/to-do.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TODOS } from '../mocked';
+
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent implements OnInit {
+
+  todos = TODOS;
 
   constructor() { }
 
